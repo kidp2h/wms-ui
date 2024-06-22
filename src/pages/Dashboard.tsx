70e06c01469e183ff7a501b66ae56dc397931d8e
kidp2h <nthinh.dev@gmail.com>
2024-06-22 11:05:43 +0700

    fix employee schedule

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,26 +1,5 @@
-import { ProjectSchedule } from '@/components/dashboard/ProjectSchedule';
-import { Flex, Tabs, TabsProps } from 'antd';
+import React from 'react';
 
 export const Dashboard = () => {
-  const items: TabsProps['items'] = [
-    {
-      key: '1',
-      label: 'Chấm công',
-      children: <ProjectSchedule employeeId={null} isAdmin={false} />,
-    },
-  ];
-  const onChange = () => {};
-  return (
-    <>
-      <Flex>
-        <Tabs
-          type='card'
-          className='w-full'
-          defaultActiveKey='1'
-          items={items}
-          onChange={onChange}
-        />
-      </Flex>
-    </>
-  );
+  return <div>Dashboard</div>;
 };
